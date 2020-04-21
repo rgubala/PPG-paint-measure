c093696a29afd129a2a11eff8d563982e0aff977
devMatthes <jaciubekmateusz@gmail.com>
2020-04-21 21:21:58 +0200

    Distance between 2 points

--- app/src/main/java/com/example/arcore_measure/MainActivity.java
+++ app/src/main/java/com/example/arcore_measure/MainActivity.java
@@ -1,29 +1,136 @@
 package com.example.arcore_measure;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
 import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static android.graphics.Color.*;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Scene.OnUpdateListener {
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     // Set to true ensures requestInstall() triggers installation if necessary.
     private boolean mUserRequestedInstall = true;
     private Session mSession;
+    private ArFragment arFragment;
+    private ArrayList<AnchorNode> currentAnchorNode = new ArrayList<>();
+    private TextView tvDistance;
+    ModelRenderable cubeRenderable;
+    private ArrayList<Anchor> currentAnchor = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            Toast.makeText(getApplicationContext(), "Device not supported", Toast.LENGTH_LONG).show();
+        }
+
         setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        tvDistance = findViewById(R.id.tvDistance);
+
+        initModel();
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            if (cubeRenderable == null){
+                return;
+            }
+
+            //creating the anchor
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+            //clearAnchor();
+
+            currentAnchor.add(anchor);
+            currentAnchorNode.add(anchorNode);
+
+            //creating the node
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+            node.setRenderable(cubeRenderable);
+            node.setParent(anchorNode);
+            arFragment.getArSceneView().getScene().addOnUpdateListener(this);
+            arFragment.getArSceneView().getScene().addChild(anchorNode);
+            node.select();
+        });
+    }
+
+    public boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        String openGlVersionString = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
+                .getDeviceConfigurationInfo()
+                .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL 3.0 or later");
+            Toast.makeText(activity, "Sceneform requires OpenGL 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+
+    private void initModel() {
+        MaterialFactory.makeOpaqueWithColor(this, new com.google.ar.sceneform.rendering.Color(RED))
+                .thenAccept(material -> {
+                    Vector3 vector3 = new Vector3(0.05f, 0.01f, 0.01f);
+                    cubeRenderable = ShapeFactory.makeCube(vector3, Vector3.zero(), material);
+                    cubeRenderable.setShadowCaster(false);
+                    cubeRenderable.setShadowReceiver(false);
+                });
+    }
+
+    private void clearAnchor() {
+        currentAnchor.clear();
+        for(AnchorNode anchorNode : currentAnchorNode) {
+            if (currentAnchorNode != null) {
+                arFragment.getArSceneView().getScene().removeChild(anchorNode);
+                anchorNode.getAnchor().detach();
+                anchorNode.setParent(null);
+            }
+        }
+        currentAnchorNode.clear();
     }
 
     @Override
@@ -55,12 +162,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
                     .show();
             return;
-        }   catch (UnavailableArcoreNotInstalledException e) {
-                e.printStackTrace();
-            } catch (UnavailableSdkTooOldException e) {
-                e.printStackTrace();
-            } catch (UnavailableApkTooOldException e) {
-                e.printStackTrace();
+        } catch (UnavailableArcoreNotInstalledException e) {
+            e.printStackTrace();
+        } catch (UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        } catch (UnavailableApkTooOldException e) {
+            e.printStackTrace();
         }
         return;  // mSession is still null.
     }
@@ -75,6 +182,25 @@ public class MainActivity extends AppCompatActivity {
                 CameraPermissionHelper.launchPermissionSettings(this);
             }
             finish();
+        }
+    }
+
+    @Override
+    public void onUpdate(FrameTime frameTime) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        Log.d("API123", "onUpdateFrame...current anchor node " + (currentAnchorNode == null));
+        
+        if (currentAnchorNode.size() == 2) {
+            Vector3 node1 = currentAnchorNode.get(1).getWorldPosition();
+            Vector3 node0 = currentAnchorNode.get(0).getWorldPosition();
+
+            float dx = node0.x - node1.x;
+            float dy = node0.y - node1.y;
+            float dz = node0.z - node1.z;
+
+            float distanceMeters = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+            tvDistance.setText("Distance : " + distanceMeters + " metres");
         }
     }
 }
